9ac1a0fa963d24cbb032467a9c894f3f786ed781
ossianhempel <hemposse@hotmail.com>
2024-12-07 17:01:51 +0100

    remove deployment code and use it as flow only

--- flows/fpl_pipeline.py
+++ flows/fpl_pipeline.py
@@ -1,15 +1,11 @@
+import datetime
+import pendulum
 import os
 import requests
 import json
-from datetime import timedelta
 from prefect import flow, task
-from prefect.tasks import task_input_hash
-from prefect.utilities.filesystem import Files
-from typing import Tuple
-import pendulum
-from prefect.filesystems import LocalFileSystem
 
-# get secrets - in Prefect you might want to use blocks for these
+# get secrets
 MINIO_ENDPOINT_FPL = os.getenv("MINIO_ENDPOINT_FPL")
 MINIO_ACCESS_KEY_FPL = os.getenv("MINIO_ACCESS_KEY_FPL")
 MINIO_SECRET_KEY_FPL = os.getenv("MINIO_SECRET_KEY_FPL")
@@ -22,49 +18,40 @@ PG_DATABASE_FPL = os.getenv("PG_DATABASE_FPL")
 PG_TABLE_NAME_GW_FPL = os.getenv("PG_TABLE_NAME_GW_FPL")
 PG_TABLE_NAME_FIXTURES_FPL = os.getenv("PG_TABLE_NAME_FIXTURES_FPL")
 
-@task(retries=1, cache_key_fn=task_input_hash, cache_expiration=timedelta(hours=1))
+@task
 def download_file(url: str, file_path: str) -> str:
-    """Download a file from a URL and save it locally."""
     response = requests.get(url)
     response.raise_for_status()
-    
     with open(file_path, "wb") as f:
         f.write(response.content)
-    
     return file_path
 
-@task(retries=1)
+@task
 def upload_file_to_fastapi(endpoint: str, source: str, file_path: str, filename: str) -> None:
-    """Upload a file to FastAPI using multipart/form-data."""
     url = f"http://os8w0ss0c4kwoogk8o44csg4.65.108.88.160.sslip.io/upload/{source}"
     with open(file_path, "rb") as f:
         files = {"file": (filename, f)}
         response = requests.post(url, files=files)
         response.raise_for_status()
-        
         if response.status_code != 200:
             raise Exception(f"Failed to upload {filename}. Status Code: {response.status_code}")
 
-@task(retries=1)
+@task
 def ingest_to_postgres(source: str) -> None:
-    """Trigger data ingestion to PostgreSQL."""
     url = "http://os8w0ss0c4kwoogk8o44csg4.65.108.88.160.sslip.io/ingest/" + source
     headers = {"Content-Type": "application/json"}
     data = {"source": source}
     
     response = requests.post(url, headers=headers, json=data)
     response.raise_for_status()
-    
     if response.status_code != 200:
         raise Exception(f"Failed to ingest {source}. Status Code: {response.status_code}")
 
-@flow(name="fpl_data_pipeline", retries=1)
+@flow(name="fpl_data_pipeline")
 def fpl_pipeline():
-    """Main flow for FPL data pipeline."""
-    # Define file paths and URLs
+    # URLs and file paths
     url_gw = "https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2024-25/gws/merged_gw.csv"
     url_fixtures = "https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2024-25/fixtures.csv"
-    
     gw_file_path = "/tmp/merged_gw_24_25.csv"
     fixtures_file_path = "/tmp/fixtures_24_25.csv"
 
@@ -81,5 +68,4 @@ def fpl_pipeline():
     ingest_to_postgres("fixtures")
 
 if __name__ == "__main__":
-    # For local testingn
     fpl_pipeline()
